5937d136b1aa53fc06bd1225b02f086a62d13289
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-07-07 15:46:06 +0300

    Add build script for OpenSSL 1.1.x.

---
+++ 1.1.x/build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+OPENSSL=openssl-1.1.1c
+HOST_PLATFORM=darwin-x86_64
+
+ANDROID_NDK_HOME=~/android-ndk-r19c; export ANDROID_NDK_HOME
+
+PATH_ORIG=$PATH
+
+BUILD_ARCH=armeabi-v7a
+
+PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_PLATFORM/arm-linux-androideabi/bin:$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_PLATFORM/bin:$PATH_ORIG; export PATH
+
+mkdir -p $BUILD_ARCH && cd $BUILD_ARCH && tar -xzf ../$OPENSSL.tar.gz && cd $OPENSSL && \
+./Configure android-arm --target=armv7a-linux-androideabi -Wl,--fix-cortex-a8 -fPIC -D__ANDROID_API__=16 && \
+make && cd ../..
+
+if [[ $? -ne 0 ]]; then
+    exit $?
+fi
+
+BUILD_ARCH=arm64-v8a
+
+PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_PLATFORM/aarch64-linux-android/bin:$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_PLATFORM/bin:$PATH_ORIG; export PATH
+
+mkdir -p $BUILD_ARCH && cd $BUILD_ARCH && tar -xzf ../$OPENSSL.tar.gz && cd $OPENSSL && \
+./Configure android-arm64 -fPIC -D__ANDROID_API__=21 && \
+make && cd ../..
+
+if [[ $? -ne 0 ]]; then
+    exit $?
+fi
