7c1cc31e7b73a66afdb5280e2587ef8ebb9009ee
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-07-31 16:09:04 +0300

    Add some quoting in scripts where appropriate.

--- 1.0.x/build.sh
+++ 1.0.x/build.sh
@@ -2,13 +2,13 @@
 
 OPENSSL=openssl-1.0.2s
 
-ANDROID_NDK_ROOT=~/android-ndk-r10e; export ANDROID_NDK_ROOT
+export ANDROID_NDK_ROOT=~/android-ndk-r10e
 
 BUILD_ARCH=armeabi-v7a
 
 . ./Setenv-android-arm.sh
 
-mkdir -p $BUILD_ARCH && cd $BUILD_ARCH && tar -xzf ../$OPENSSL.tar.gz && cd $OPENSSL && ./Configure shared android && make CALC_VERSIONS="SHLIB_COMPAT=; SHLIB_SOVER=" && cd ../..
+mkdir -p "$BUILD_ARCH" && cd "$BUILD_ARCH" && tar -xzf "../$OPENSSL.tar.gz" && cd "$OPENSSL" && ./Configure shared android && make CALC_VERSIONS="SHLIB_COMPAT=; SHLIB_SOVER=" && cd ../..
 
 if [[ $? -ne 0 ]]; then
     exit $?
@@ -18,7 +18,7 @@ BUILD_ARCH=arm64-v8a
 
 . ./Setenv-android-arm64.sh
 
-mkdir -p $BUILD_ARCH && cd $BUILD_ARCH && tar -xzf ../$OPENSSL.tar.gz && cd $OPENSSL && ./Configure shared android && make CALC_VERSIONS="SHLIB_COMPAT=; SHLIB_SOVER=" && cd ../..
+mkdir -p "$BUILD_ARCH" && cd "$BUILD_ARCH" && tar -xzf "../$OPENSSL.tar.gz" && cd "$OPENSSL" && ./Configure shared android && make CALC_VERSIONS="SHLIB_COMPAT=; SHLIB_SOVER=" && cd ../..
 
 if [[ $? -ne 0 ]]; then
     exit $?
--- 1.1.x/build.sh
+++ 1.1.x/build.sh
@@ -3,15 +3,15 @@
 OPENSSL=openssl-1.1.1g
 HOST_PLATFORM=darwin-x86_64
 
-ANDROID_NDK_HOME=~/android-ndk-r19c; export ANDROID_NDK_HOME
+export ANDROID_NDK_HOME=~/android-ndk-r19c
 
-PATH_ORIG=$PATH
+PATH_ORIG="$PATH"
 
 BUILD_ARCH=armeabi-v7a
 
-PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_PLATFORM/arm-linux-androideabi/bin:$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_PLATFORM/bin:$PATH_ORIG; export PATH
+export PATH="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_PLATFORM/arm-linux-androideabi/bin:$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_PLATFORM/bin:$PATH_ORIG"
 
-mkdir -p $BUILD_ARCH && cd $BUILD_ARCH && tar -xzf ../$OPENSSL.tar.gz && cd $OPENSSL && \
+mkdir -p "$BUILD_ARCH" && cd "$BUILD_ARCH" && tar -xzf "../$OPENSSL.tar.gz" && cd "$OPENSSL" && \
 ./Configure android-arm --target=armv7a-linux-androideabi -Wl,--fix-cortex-a8 -fPIC -D__ANDROID_API__=16 && \
 make SHLIB_VERSION_NUMBER= SHLIB_EXT=_1_1.so && cd ../..
 
@@ -21,9 +21,9 @@ fi
 
 BUILD_ARCH=arm64-v8a
 
-PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_PLATFORM/aarch64-linux-android/bin:$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_PLATFORM/bin:$PATH_ORIG; export PATH
+export PATH="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_PLATFORM/aarch64-linux-android/bin:$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_PLATFORM/bin:$PATH_ORIG"
 
-mkdir -p $BUILD_ARCH && cd $BUILD_ARCH && tar -xzf ../$OPENSSL.tar.gz && cd $OPENSSL && \
+mkdir -p "$BUILD_ARCH" && cd "$BUILD_ARCH" && tar -xzf "../$OPENSSL.tar.gz" && cd "$OPENSSL" && \
 ./Configure android-arm64 -fPIC -D__ANDROID_API__=21 && \
 make SHLIB_VERSION_NUMBER= SHLIB_EXT=_1_1.so && cd ../..
 
